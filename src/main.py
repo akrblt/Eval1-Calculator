c795a3b3f7418e9038a32aa63570b50704ad03fe
akrblt <a.karabulut070@gmail.com>
2024-12-02 11:57:01 +0100

    fixed les test

--- src/main.py
+++ src/main.py
@@ -1,62 +1,55 @@
-class Calculator:
-    def __int__(self):
-        self.operand1=None
-        self.operand2=None
-        self.operator=None
-
-
-
-
-
-
-
-    def ask_user_input(self):
-    # Get first operand
-        self.operand1 = self.ask_user_float_input("Enter the first operand: ")
-
-    # Get operator
-        self.operator = input("Enter an operator (+, -, *, /, **): ")
-
-    # Get second operand
-        self.operand2 = self.ask_user_float_input("Enter the second operand: ")
-
-
-    @staticmethod
-    def ask_user_float_input(prompt):
-        while True:
-            try:
-                return float(input(prompt))
-            except ValueError:
-                print("Invalid input. Please enter a valid number.")
-
-    def calculate(self):
-    # Perform the operation based on the operator
-        match self.operator:
-            case '+':
-                return self.operand1 + self.operand2
-            case '-':
-                return self.operand1 - self.operand2
-            case '*':
-                return self.operand1 * self.operand2
-            case '/':
-                if self.operand2 == 0:
-                    print("Error: Division by zero is undefined.")
-                    return None
-                return self.operand1 / self.operand2
-            case '**':
-                return self.operand1 ** self.operand2
-            case _:
-                print("Invalid  operator.")
-                return None
-
-    def display_result(self,result):
-        print(f"{self.operand1} {self.operator} {self.operand2} = {result}")
-
+from src.MathRequest import MathRequest
 def main():
-    calculator=Calculator()
-    calculator.ask_user_input()
-    result=calculator.calculate()
-    calculator.display_result(result)
+    math_request = ask_user_input()
+    math_request.set_res(calculate(math_request))
+    display_result(math_request)
 
-# Run the program
+def ask_user_input() -> MathRequest:
+    # Get first operand from the user
+    ope1 = ask_user_float_input("Enter the first operand: ")
+
+    # Get the operator from the user
+    oper = input("Enter an operator (+, -, *, /, ^): ")
+
+    # Get second operand from the user
+    ope2 = ask_user_float_input("Enter the second operand: ")
+
+    return MathRequest(ope1, oper, ope2)
+
+def ask_user_float_input(msg):
+    return float(input(msg))
+
+def calculate(math_request):
+    # Perform the operation based on the operator
+    ope1 = math_request.get_ope1()
+    oper = math_request.get_oper()
+    ope2 = math_request.get_ope2()
+
+    match oper:
+        case '+':
+            res = ope1 + ope2
+        case '-':
+            res = ope1 - ope2
+        case '*':
+            res = ope1 * ope2
+        case '/':
+            if ope2 == 0:
+                print("Error: Division by zero is undefined.")
+                return
+            res = ope1 / ope2
+        case '^':
+            res = 1
+            for count in range(int(ope1)):
+                res = res * ope2
+        case _:
+            print("Invalid operator.")
+            return
+    return res
+
+def display_result(math_request):
+    # Print the result
+        print(math_request.to_string())
+
+
+# Call the main function to run the program
 main()
